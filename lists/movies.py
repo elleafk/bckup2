7a388b6c9faadd01e5dd1a2ac324f8cb11544dbe
elz <jnellesalamat@gmail.com>
2026-07-17 21:47:01 +0800

    listnumber4

---
+++ lists/movies.py
@@ -0,0 +1,26 @@
+movies = []
+
+totalmovies = int(input("Enter the number of movies: "))
+
+for i in range(totalmovies):
+    movie = input(f"Movie title {i+1}: ")
+    movies.append(movie)
+    
+print("\nFavorite Movies:")
+for num, movie in enumerate(movies, start=1):
+    print(f"{num}. {movie}")
+    
+print("\nTotal movies: ", len(movies))
+print("First movie: ", movies[0])
+print("Last movie: ", movies[-1])
+    
+view = int(input("\nEnter the number of the movie you want to view: "))
+
+if view <=len(movies):
+    print(f"You selected: {movies[view-1]}")
+else:
+    print("invalid selection")
+
+
+
+
