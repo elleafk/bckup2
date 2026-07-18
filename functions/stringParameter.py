669b31a9773c59dc0a825c687bc9fc8732ed72d0
elz <jnellesalamat@gmail.com>
2026-07-18 15:02:12 +0800

    functions2

---
+++ functions/stringParameter.py
@@ -0,0 +1,11 @@
+def greet (name, age, address):
+    print(f"Hello {name}!",
+          f"This year you are currently {age} years old.",
+          f"You are living in {address}.",
+          "Welcome to this program!")
+    
+print("User 1:")
+greet("Janelle", 19, "Poblacion, Paombong, Bulacan")
+print("\nUser 2:")
+greet("Rain", 19, "Hagonoy, Bulacan")
+    
