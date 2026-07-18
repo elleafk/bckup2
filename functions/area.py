a410b5787b87e1ddf30bf29b718cc49b9f1fd31f
elz <jnellesalamat@gmail.com>
2026-07-18 15:19:15 +0800

    functions4

---
+++ functions/area.py
@@ -0,0 +1,12 @@
+def rectangleArea(length,width):
+    print(f"Length: {length}")
+    print(f"Width: {width}")
+    area = length * width
+    print(f"Area: {area}")
+    
+print("\nRectangle Area 1:")
+rectangleArea(5,4)
+print("\nRectangle Area 2:")
+rectangleArea(10,6)
+print("\nRectangle Area 3:")
+rectangleArea(20,16)
